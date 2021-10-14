2334052ade245cf7096f01e058ddb25da1f7c33d
Ivan Kavtarev <ivan.kavtarev@mail.ru>
2021-10-14 12:46:34 +0300

    done, need refactoring

--- client/src/ClassTextArea/ClassTextAreaBehaviour.tsx
+++ client/src/ClassTextArea/ClassTextAreaBehaviour.tsx
@@ -1,48 +1,53 @@
-import React from 'react'
-import { ClassTextAreaTemplate } from './ClassTextAreaTemplate'
+import React from "react";
+import { ClassTextAreaTemplate } from "./ClassTextAreaTemplate";
 
 interface Props {
-    textValue: string
-    inputOnChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void
-    title: string
-    isRequired: boolean
+  textValue: string;
+  inputOnChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
+  title: string;
+  isRequired: boolean;
+  charactersLimit: number;
 }
 interface State {
-    areaHeight: string
-    textValue: string
+  areaHeight: string;
+  textValue: string;
 }
 
 export class ClassTextAreaBehaviour extends React.Component<Props, State> {
-    inputRef = React.createRef<HTMLInputElement>()
-    constructor(props: Props) {
-        super(props)
-        this.state = {
-            areaHeight: '36px',
+  inputRef = React.createRef<HTMLInputElement>();
 
-            textValue: '',
-        }
-        this.onInputChange = this.onInputChange.bind(this)
-    }
-    componentDidUpdate(prevProps: Props, prevState: State) {
-        if (this.props.textValue !== prevProps.textValue) {
-            this.setState({ areaHeight: `${this.inputRef.current?.scrollHeight}px` })
-        }
-    }
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      areaHeight: "36px",
 
-    render(): JSX.Element {
-        const { areaHeight, textValue } = this.state
-        const { title, isRequired } = this.props
-        return React.createElement(ClassTextAreaTemplate, {
-            title,
-            areaHeight,
-            textValue,
-            inputRef: this.inputRef,
-            isRequired,
-            onInputChange: this.onInputChange,
-        })
+      textValue: "",
+    };
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  componentDidUpdate(prevProps: Props, prevState: State) {
+    if (this.props.textValue !== prevProps.textValue) {
+      this.setState({ areaHeight: `${this.inputRef.current?.scrollHeight}px` });
     }
-    onInputChange(e: React.ChangeEvent<HTMLTextAreaElement>) {
-        this.setState({ textValue: e.target.value, areaHeight: 'auto' })
-        this.props.inputOnChange(e)
-    }
+  }
+
+  render(): JSX.Element {
+    const { areaHeight, textValue } = this.state;
+    const { title, isRequired, charactersLimit } = this.props;
+    return React.createElement(ClassTextAreaTemplate, {
+      title,
+      charactersLimit,
+      areaHeight,
+      textValue,
+      inputRef: this.inputRef,
+      isRequired,
+      onInputChange: this.onInputChange,
+    });
+  }
+
+  public onInputChange(e: React.ChangeEvent<HTMLTextAreaElement>) {
+    this.setState({ textValue: e.target.value, areaHeight: "auto" });
+    this.props.inputOnChange(e);
+  }
 }
--- client/src/ClassTextArea/ClassTextAreaTemplate.tsx
+++ client/src/ClassTextArea/ClassTextAreaTemplate.tsx
@@ -1,37 +1,43 @@
-import React from 'react'
-import './TextArea.scss'
+import React from "react";
+import "./TextArea.scss";
 
 interface Props {
-    inputRef: any
-    areaHeight: string
-    title: string
-    textValue: string
-    isRequired: boolean
-    onInputChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void
+  inputRef: any;
+  areaHeight: string;
+  title: string;
+  textValue: string;
+  isRequired: boolean;
+  charactersLimit: number;
+  onInputChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
 }
 
 export function ClassTextAreaTemplate({
-    inputRef,
-    areaHeight,
-    title,
-    textValue,
-    onInputChange,
-    isRequired,
+  inputRef,
+  areaHeight,
+  title,
+  textValue,
+  onInputChange,
+  isRequired,
+  charactersLimit,
 }: Props) {
-    const ROOT_CLASS = 'textarea'
-    return (
-        <textarea
-            placeholder={
-                isRequired ? 'Обязательно для заполнения' : 'Не обязательно для заполнения'
-            }
-            className={`${ROOT_CLASS}__item`}
-            ref={inputRef}
-            style={{ height: areaHeight }}
-            rows={1}
-            id={title}
-            defaultValue={textValue || ''}
-            onChange={onInputChange}
-            required
-        ></textarea>
-    )
+  const ROOT_CLASS = "textarea";
+  return (
+    <textarea
+      placeholder={
+        isRequired
+          ? "Обязательно для заполнения"
+          : charactersLimit
+          ? "Не обязательно для заполнения"
+          : "Введите ключевые слова"
+      }
+      className={`${ROOT_CLASS}__item`}
+      ref={inputRef}
+      style={{ height: areaHeight }}
+      rows={1}
+      id={title}
+      defaultValue={textValue || ""}
+      onChange={onInputChange}
+      required
+    ></textarea>
+  );
 }
