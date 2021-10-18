125e35b1c8a3b0a8c8ab1b91c169c7304690cb98
Ivan Kavtarev <ivan.kavtarev@mail.ru>
2021-10-18 16:52:01 +0300

    minor trash

--- client/src/Input/InputBehavior.tsx
+++ client/src/Input/InputBehavior.tsx
@@ -1,60 +1,57 @@
-import React from 'react'
-import { InputTemplate } from './InputTemplate'
+import React from "react";
+import { InputTemplate } from "./InputTemplate";
 
 interface Props {
-    charactersLimit?: number
-    title: string
-    error?: boolean
-    isRequired: boolean
+  charactersLimit?: number;
+  title: string;
+  error?: boolean;
+  isRequired: boolean;
 }
 
 interface State {
-    textValue: string
-    howManyLeft: number
-    charactersLimit: number
-    areaHeight: number
+  textValue: string;
+  howManyLeft: number;
+  charactersLimit: number;
+  areaHeight: number;
 }
 
-//how many left ???
-
 export class InputBehaviour extends React.Component<Props, State> {
-    constructor(props: Props) {
-        super(props)
-        this.state = {
-            textValue: '',
-            howManyLeft: this.props.charactersLimit || 0,
-            charactersLimit: this.props.charactersLimit || 0,
-            areaHeight: 36,
-        }
-        this.onInputChange = this.onInputChange.bind(this)
-    }
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      textValue: "",
+      howManyLeft: this.props.charactersLimit || 0,
+      charactersLimit: this.props.charactersLimit || 0,
+      areaHeight: 36,
+    };
+    this.onInputChange = this.onInputChange.bind(this);
+  }
 
-    render(): JSX.Element {
-        const { textValue, howManyLeft, areaHeight, charactersLimit } = this.state
-        let { title, error, isRequired } = this.props
-        error = error ? error : false
-        return React.createElement(InputTemplate, {
-            textValue,
-            title,
-            howManyLeft,
-            error,
-            areaHeight,
-            isRequired,
-            charactersLimit,
-            onInputChange: this.onInputChange,
-            setText: this.setText,
-        })
-    }
-    public onInputChange(e: React.ChangeEvent<HTMLTextAreaElement>) {
-        console.log(e.target.scrollHeight)
-        this.setState({
-            textValue: e.target.value,
-            howManyLeft: this.props.charactersLimit
-                ? this.state.charactersLimit - e.target.value.length
-                : this.state.charactersLimit + e.target.value.length,
+  render(): JSX.Element {
+    const { textValue, howManyLeft, areaHeight, charactersLimit } = this.state;
+    let { title, error, isRequired } = this.props;
+    error = error ? error : false;
+    return React.createElement(InputTemplate, {
+      textValue,
+      title,
+      howManyLeft,
+      error,
+      areaHeight,
+      isRequired,
+      charactersLimit,
+      onInputChange: this.onInputChange,
+      setText: this.setText,
+    });
+  }
+  public onInputChange(e: React.ChangeEvent<HTMLTextAreaElement>) {
+    this.setState({
+      textValue: e.target.value,
+      howManyLeft: this.props.charactersLimit
+        ? this.state.charactersLimit - e.target.value.length
+        : this.state.charactersLimit + e.target.value.length,
 
-            areaHeight: e.target.scrollHeight,
-        })
-    }
-    public setText() {}
+      areaHeight: e.target.scrollHeight,
+    });
+  }
+  public setText() {}
 }
